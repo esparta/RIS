20b42c44f1d10b89cbc19b72dcf4c80d940be847
Espartaco Palma <esparta@gmail.com>
2013-10-27 20:29:01 -0600

    updated sample

--- sample.py
+++ sample.py
@@ -1,19 +1,23 @@
+from __future__ import print_function 
 from risparser import RISParser
 from collections import defaultdict
 
 def main():
     pubyear = defaultdict(int)
-    filename = "data/bigdata.keyword.ris"
+    filename = "data/hadoop-mapreduce.ris"
 
     with RISParser(filename) as citations:
         for c in citations:
             p = c.getvalue("PY")
-            print c.getvalue("AU")
-            if p and p.year != None:     
-                pubyear[p.year] += 1
+            #print c.getvalue("AU")    
+            pubyear[p.year] += 1
 
+    refs = 0
     for k,v in pubyear.items():
-        print k,v
+        print(k,v)
+        refs += v
+
+    print("Total references: {0}".format(refs))
     #print pubyear
 
 if __name__ == "__main__":
